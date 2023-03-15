123e078a3d8bfb5079f812d7955c62dcdc314185
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-03-15 00:18:40 -0400

    Started making  the required api routes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -67,6 +67,7 @@ const renderActiveNote = () => {
 };
 
 const handleNoteSave = () => {
+  console.log("note save handling");
   const newNote = {
     title: noteTitle.value,
     text: noteText.value,
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+const uuid = require("uuid");
+const savedNotes = require("./db/db");
 const PORT = process.env.PORT || 5000;
 // const fetch = require("node-fetch");
 
 app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const newNote = {
+      id: uuid.v4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.send("Note added sucessfully");
+    });
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
